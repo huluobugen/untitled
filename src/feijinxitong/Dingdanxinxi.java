2563a38117b1fe54da3fb191a2b73355ce59ebab
huluobugen <1821044807@qq.com>
2021-01-11 16:16:25 +0800

    框架

---
+++ src/feijinxitong/Dingdanxinxi.java
@@ -0,0 +1,31 @@
+package feijinxitong;
+
+public class Dingdanxinxi {
+    private float jiage;//单价
+    private float jcs;//机场税
+    private float zj;//总价
+
+    public float getJiage() {
+        return jiage;
+    }
+
+    public void setJiage(float jiage) {
+        this.jiage = jiage;
+    }
+
+    public float getJcs() {
+        return jcs;
+    }
+
+    public void setJcs(float jcs) {
+        this.jcs = jcs;
+    }
+
+    public float getZj() {
+        return zj;
+    }
+
+    public void setZj(float zj) {
+        this.zj = zj;
+    }
+}
---
+++ src/feijinxitong/Hangbanxinxi.java
@@ -0,0 +1,41 @@
+package feijinxitong;
+
+public class Hangbanxinxi {
+    private String cfcs;//出发城市
+    private String ddcs;//到达城市
+    private String cfrq;//出发日期
+    private String cklx;//乘客类型
+
+    public String getCfcs() {
+        return cfcs;
+    }
+
+    public void setCfcs(String cfcs) {
+        this.cfcs = cfcs;
+    }
+
+    public String getDdcs() {
+        return ddcs;
+    }
+
+    public void setDdcs(String ddcs) {
+        this.ddcs = ddcs;
+    }
+
+    public String getCfrq() {
+        return cfrq;
+    }
+
+    public void setCfrq(String cfrq) {
+        this.cfrq = cfrq;
+    }
+
+    public String getCklx() {
+        return cklx;
+    }
+
+    public void setCklx(String cklx) {
+        this.cklx = cklx;
+    }
+}
+
